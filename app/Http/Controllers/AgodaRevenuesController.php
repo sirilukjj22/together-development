d44883c03ad8a30158d87f04d822cde0ad1cbf47
จักรกฤษณ์ เสียงอ่อน <69667394+Jukkrit-Seangon@users.noreply.github.com>
2024-06-01 12:09:20 +0700

    test

--- app/Http/Controllers/AgodaRevenuesController.php
+++ app/Http/Controllers/AgodaRevenuesController.php
@@ -11,17 +11,13 @@ use Illuminate\Support\Facades\DB;
 
 class AgodaRevenuesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $agoda_outstanding = Revenue_credit::leftjoin('revenue', 'revenue_credit.revenue_id', 'revenue.id')
             // ->whereMonth('revenue.date', $exp[1])->whereYear('revenue.date', $exp[0])
             ->where('revenue_credit.status', 5)
-            ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out', 
+            ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out',
             'revenue_credit.revenue_type', 'revenue_credit.agoda_charge', 'revenue_credit.receive_payment',
             'revenue_credit.agoda_outstanding', 'revenue_credit.sms_revenue')
             ->get();
@@ -47,14 +43,14 @@ class AgodaRevenuesController extends Controller
         $agoda_revenue = SMS_alerts::where('status', 5)->get();
 
         return view('agoda.list_agoda', compact('agoda_revenue'));
-    } 
+    }
 
     public function index_receive($id)
     {
         $agoda_outstanding = Revenue_credit::leftjoin('revenue', 'revenue_credit.revenue_id', 'revenue.id')
             // ->whereMonth('revenue.date', $exp[1])->whereYear('revenue.date', $exp[0])
             ->where('revenue_credit.status', 5)
-            ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out', 
+            ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out',
             'revenue_credit.revenue_type', 'revenue_credit.agoda_charge', 'revenue_credit.receive_payment',
             'revenue_credit.agoda_outstanding', 'revenue_credit.sms_revenue')
             ->orderBy('revenue_credit.agoda_check_in', 'asc')->get();
@@ -80,7 +76,7 @@ class AgodaRevenuesController extends Controller
         $agoda_outstanding = Revenue_credit::leftjoin('revenue', 'revenue_credit.revenue_id', 'revenue.id')
             // ->whereMonth('revenue.date', $exp[1])->whereYear('revenue.date', $exp[0])
             ->where('revenue_credit.status', 5)
-            ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out', 
+            ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out',
             'revenue_credit.revenue_type', 'revenue_credit.agoda_charge', 'revenue_credit.receive_payment',
             'revenue_credit.agoda_outstanding', 'revenue_credit.sms_revenue')
             ->orderBy('revenue_credit.agoda_check_in', 'asc')->get();
@@ -113,7 +109,7 @@ class AgodaRevenuesController extends Controller
 
             foreach ($request->receive_id as $key => $value) {
                 $check_data = Revenue_credit::where('id', $value)->first();
-    
+
                 if ($check_data->receive_payment == 0) {
                     Revenue_credit::where('id', $value)->update([
                         'date_receive' => date('Y-m-d'),
@@ -121,12 +117,12 @@ class AgodaRevenuesController extends Controller
                         'sms_revenue' => $request->revenue_id
                     ]);
                 }
-    
+
                 SMS_alerts::where('id', $request->revenue_id)->update([
                     'status_receive_agoda' => 1
                 ]);
             }
-            
+
             return response()->json([
                 'status' => 200,
                 'message' => "บันทึกข้อมูลสำเร็จ"
@@ -143,7 +139,7 @@ class AgodaRevenuesController extends Controller
     public function select_agoda_received($id) {
 
         $agoda_received = Revenue_credit::where('sms_revenue', $id)->where('revenue_credit.status', 5)->where('receive_payment', 1)
-        ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out', 
+        ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out',
         'revenue_credit.revenue_type', 'revenue_credit.agoda_charge', 'revenue_credit.receive_payment',
         'revenue_credit.agoda_outstanding', 'revenue_credit.sms_revenue')->orderBy('revenue_id', 'asc')->get();
 
@@ -156,7 +152,7 @@ class AgodaRevenuesController extends Controller
     public function select_agoda_outstanding($id) {
 
         $agoda_outstanding = Revenue_credit::where('id', $id)->where('revenue_credit.status', 5)
-        ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out', 
+        ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out',
             'revenue_credit.revenue_type', 'revenue_credit.agoda_charge', 'revenue_credit.receive_payment',
             'revenue_credit.agoda_outstanding', 'revenue_credit.sms_revenue')->first();
 
@@ -169,7 +165,7 @@ class AgodaRevenuesController extends Controller
     public function status_agoda_receive($status) {
 
         $agoda_received = Revenue_credit::where('revenue_credit.status', 5)->where('receive_payment', $status)
-        ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out', 
+        ->select('revenue_credit.id', 'revenue_credit.batch', 'revenue_credit.agoda_check_in', 'revenue_credit.agoda_check_out',
         'revenue_credit.revenue_type', 'revenue_credit.agoda_charge', 'revenue_credit.receive_payment',
         'revenue_credit.agoda_outstanding', 'revenue_credit.sms_revenue')->orderBy('revenue_id', 'asc')->get();
 
