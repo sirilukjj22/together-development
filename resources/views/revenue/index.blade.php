5272ab05668e73d28cd8d4854e5dd8f9389d5d6c
siriluk jansri <sirilukjj22@gmail.com>
2024-06-17 20:51:53 +0700

    Update Revenue

--- resources/views/revenue/index.blade.php
+++ resources/views/revenue/index.blade.php
@@ -1569,6 +1569,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" onclick="revenue_store()">Save changes</button>
                     </div>
                 </div>
             </div>
