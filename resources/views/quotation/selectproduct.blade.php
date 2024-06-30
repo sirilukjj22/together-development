b021ec03178be2d84df47336eba93087a3f19ab4
จักรกฤษณ์ เสียงอ่อน <69667394+Jukkrit-Seangon@users.noreply.github.com>
2024-06-30 16:41:00 +0700

    update:pdf

--- resources/views/quotation/selectproduct.blade.php
+++ resources/views/quotation/selectproduct.blade.php
@@ -444,7 +444,7 @@
                                             $linkQR = $gethttp."://".$_SERVER['HTTP_HOST']."/Quotation/Quotation/cover/document/PDF/$id?page_shop=".@$_GET['page_shop'];
                                         @endphp
                                         <div class="mt-3">
-                                            <img src="data:image/png;base64, {!! base64_encode(QrCode::size(90)->generate($linkQR)) !!} ">
+                                            <img src="data:image/png;base64,{{DNS2D::getBarcodePNG($linkQR,'QRCODE') }}" width="90" height="90"/>
                                         </div>
                                     </div>
                                     <div class="col-lg-2 centered-content">
