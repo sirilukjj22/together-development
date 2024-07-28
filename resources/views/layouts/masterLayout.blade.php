e88bdfe3cf5cd935b183a92e0c2acd9d88460970
siriluk jansri <sirilukjj22@gmail.com>
2024-07-28 14:22:51 +0700

    Update SMS Detail

--- resources/views/layouts/masterLayout.blade.php
+++ resources/views/layouts/masterLayout.blade.php
@@ -19,6 +19,9 @@
     <link rel="stylesheet" href="{{ asset('assets/css/al.style.min.css') }}">
     <!-- project layout css file -->
     <link rel="stylesheet" href="{{ asset('assets/css/layout.c.min.css') }}">
+
+    <!-- for style -->
+    <link rel="stylesheet" href="{{ asset('assets/src/smsPage.css')}}">
 </head>
 
 <body>
@@ -254,7 +257,6 @@
             </div>
 
             <!-- Body: Footer -->
-
             <div class="body-footer">
                 <div class="container">
                     <div class="row">
@@ -308,9 +310,6 @@
 
     <!-- Jquery Page Js -->
     <script src="{{ asset('assets/js/template.js') }}"></script>
-
-    <!-- for taiwind style -->
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 
     <script>
         $(document).ready(function() {
