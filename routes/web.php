b125584681ac4572fc8a7445958d4b05bab6f14b
siriluk jansri <sirilukjj22@gmail.com>
2024-09-17 09:03:15 +0700

    Update clear cache

--- routes/web.php
+++ routes/web.php
@@ -628,5 +628,6 @@ Route::get('/clear-cache', function () {
     $exitCode = Artisan::call('config:clear');
     $exitCode = Artisan::call('cache:clear');
     $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('view:clear');
     return 'DONE'; //Return anything
 });
