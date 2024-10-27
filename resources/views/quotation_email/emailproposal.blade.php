8e070f2c043a1911a08717fad5d99402eb99c87e
จักรกฤษณ์ เสียงอ่อน <69667394+Jukkrit-Seangon@users.noreply.github.com>
2024-10-27 16:38:48 +0700

    update

--- resources/views/quotation_email/emailproposal.blade.php
+++ resources/views/quotation_email/emailproposal.blade.php
@@ -6,7 +6,7 @@
 <body>
     <p>{!! $Data['detail'] !!}</p>
     <p>ความคิดเห็น: {{ $Data['comment'] }}</p>
-    <a href="{{ asset($Data['pdf'].".pdf") }}">[เอกสาร]</a>
+    <a href="{{ asset($Data['pdf'].".pdf") }}">[เอกสาร]</a><br>
     <img src="{{ asset('assets2/images/tgt-01.jpg') }}" style="400px"/>
     <!-- เพิ่มรายละเอียดใบเสนอราคาเพิ่มเติมตามต้องการ -->
 </body>
