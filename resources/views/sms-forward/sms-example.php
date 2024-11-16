0260c16f20ebbc0b93ddbe84a45e1d187bb3add6
siriluk jansri <sirilukjj22@gmail.com>
2024-11-16 08:21:24 +0700

    Update SMS API

--- resources/views/sms-forward/sms-example.php
+++ resources/views/sms-forward/sms-example.php
@@ -75,15 +75,15 @@
 		$sim =  $params["sim"];
 	}
 
-	$myfile = fopen("testfile.txt", "w");
-	fwrite($myfile, pack("CCC",0xef,0xbb,0xbf));  // convert to utf8
-	fwrite($myfile, "phone=$phone\n");
-	fwrite($myfile, "text=$text\n");
-	fwrite($myfile, "sim=$sim\n");
-	if($device!=""){
-		fwrite($myfile, "device=$device\n");
-	}
-	fclose($myfile);
+	// $myfile = fopen("testfile.txt", "w");
+	// fwrite($myfile, pack("CCC",0xef,0xbb,0xbf));  // convert to utf8
+	// fwrite($myfile, "phone=$phone\n");
+	// fwrite($myfile, "text=$text\n");
+	// fwrite($myfile, "sim=$sim\n");
+	// if($device!=""){
+	// 	fwrite($myfile, "device=$device\n");
+	// }
+	// fclose($myfile);
 
 	// $phone = "027777777";
 	// $text = "105.00 บ.จากxBorder ชำระผ่านQR เข้า076355900016902 06/10@11:09";
